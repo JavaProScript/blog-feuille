66b85fe1d7c74ee6190e5d64115e244cc1d718ae
Pierre Lion <plion@dtek.fr>
2023-03-13 12:15:36 +0100

    created landing page and cleaned some css from the template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,90 +1,57 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
       <div className={styles.center}>
         <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
+          src="/images/Matrix-Still-2048x1229.jpeg"
+          alt="Matrix background"
+          width={900}
+          height={600}
           priority
         />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
       </div>
 
       <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <Link href="#"
+            className={styles.card}
+            rel="noopener noreferrer">
+          
+            <h2 className={inter.className}>
+              SSRS <span>-&gt;</span>
+            </h2>
+            <p className={inter.className}>
+              Comment j'ai utilisé SSRS et Reportbuilder pour faire des jolis tableaux.
+            </p>
+        </Link>
+
+
+
+        <Link href="#"
+            className={styles.card}
+            rel="noopener noreferrer">
           <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
+            Angular VS React <span>-&gt;</span>
+          </h2>
+          <p className={inter.className}>Et lequel privilégier selon le projet.</p>
+        </Link>
+
+        <Link href="#"
+            className={styles.card}
+            rel="noopener noreferrer">
+          <h2 className={inter.className}>
+            Top 5 films de tech <span>-&gt;</span>
           </h2>
           <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
+            Les films un peu geek avec les meilleurs scénarios à mon goût.
           </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        </Link>
       </div>
     </main>
   )
